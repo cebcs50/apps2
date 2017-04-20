19be64e5a25d739ed7af5c6387929bc9d7ee0ba6
Carol Braam <carolbraam@wcw-stud-145-109-11-7.wireless.uva.nl>
2017-04-20 13:51:31 +0200

    Initial Commit

---
+++ CarolBraam-Pset2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarolBraam-Pset2
+//
+//  Created by Carol Braam on 20-04-17.
+//  Copyright © 2017 Carol Braam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
